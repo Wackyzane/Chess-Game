e7178f7919c2d768b1cb47ca2c81027f852cc0f6
Wackyzane <karlsen.per.a@gmail.com>
2025-02-16 13:33:24 -0700

    Initial Commit

---
+++ Chess Game/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Chess_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ChessBoard = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ChessBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChessBoard
+            // 
+            this.ChessBoard.Image = ((System.Drawing.Image)(resources.GetObject("ChessBoard.Image")));
+            this.ChessBoard.Location = new System.Drawing.Point(226, 15);
+            this.ChessBoard.Name = "ChessBoard";
+            this.ChessBoard.Size = new System.Drawing.Size(450, 450);
+            this.ChessBoard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ChessBoard.TabIndex = 0;
+            this.ChessBoard.TabStop = false;
+            this.ChessBoard.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 477);
+            this.Controls.Add(this.ChessBoard);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ChessBoard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ChessBoard;
+    }
+}
+
---
+++ Chess Game/Scripts/Board.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace Chess_Game.Scripts
+{
+    enum ChessPiece
+    {
+        Empty,
+        King,
+        Queen,
+        Bishop,
+        Knight,
+        Rook,
+        Pawn
+    }
+
+    enum Player
+    {
+        None,
+        White,
+        Black
+    }
+
+    internal static class Board
+    {
+        
+        private static int[,] grid = new int[8,8];
+
+        public static void CreateBoard(Form form)
+        {
+            string imagePath = Directory.GetParent(Application.StartupPath).Parent.FullName;
+            imagePath = Path.Combine(imagePath, "Assets", "PawnW.png");
+            for (int row = 0; row < grid.GetLength(0); row++)
+            {
+                for (int col = 0; col < grid.GetLength(1); col++)
+                {
+                    PictureBox chessPiece = new PictureBox();
+
+                    chessPiece.Size = new Size(56, 56);
+                    chessPiece.Location = new Point(226, 15);
+                    chessPiece.SizeMode = PictureBoxSizeMode.StretchImage;
+                    chessPiece.Name = Convert.ToChar('A' + col).ToString() + (row + 1);
+                    chessPiece.Image = Image.FromFile(imagePath);
+
+                    form.Controls.Add(chessPiece);
+                }
+            }
+        }
+
+        public static void ResetBoard()
+        {
+            Array.Clear(grid, 0, grid.Length);
+
+            for (int column = 0; column < grid.GetLength(1); column++)
+            {
+                grid[1, column] = 0;
+                grid[7, column] = 0;
+            }
+        }
+
+    }
+}
